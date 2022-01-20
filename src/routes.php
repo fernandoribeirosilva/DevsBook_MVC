ffc6014f57762f56855171d544af0b7f8930b3cd
Fernando Ribeiro da Silva <fernando1996ribeirosilva@gmail.com>
2022-01-20 15:44:05 -0300

    adicionado links de navegação e puxando informações do usuário do banco de dados

--- src/routes.php
+++ src/routes.php
@@ -10,3 +10,10 @@ $router->post('/login', 'LoginController@signinAction');
 
 $router->get('/cadastro', 'LoginController@signup');
 $router->post('/cadastro', 'LoginController@signupAction');
+
+//$router->get('/pesquisar');
+//$route->get('/perfil');
+//$route->get('/sair');
+//$route->get('/amigos');
+//$route->get('/fotos');
+//$route->get('/config');
